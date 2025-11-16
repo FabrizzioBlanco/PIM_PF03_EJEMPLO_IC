bb96a04887b08f61a60858bb7e83f354f2c24513
the-architecto <fabrizzio@turingtec.es>
2025-11-16 16:54:51 +0100

    ADDED: Parametized Tests

--- src/test/java/com/pim/jrgs2526/pf03/AppTest.java
+++ src/test/java/com/pim/jrgs2526/pf03/AppTest.java
@@ -3,30 +3,35 @@ package com.pim.jrgs2526.pf03;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-    @Test
-    public void testCalcularAprobado() {
+    @ParameterizedTest
+    @ValueSource(ints = { 5, 7, 10 })
+    public void testCalcularAprobado(int nota) {
         App main = new App();
-        int resultado = main.calcularNota(6);
+        int resultado = main.calcularNota(nota);
         Assertions.assertEquals(1, resultado);
     }
 
-    @Test
-    public void testCalcularSuspenso() {
+    @ParameterizedTest
+    @ValueSource(ints = { 0, 2, 4 })
+    public void testCalcularSuspenso(int nota) {
         App main = new App();
-        int resultado = main.calcularNota(2);
+        int resultado = main.calcularNota(nota);
         Assertions.assertEquals(-1, resultado);
     }
 
-    @Test
-    public void testCalcularError() {
+    @ParameterizedTest
+    @ValueSource(ints = { -1, 11, 63 })
+    public void testCalcularError(int nota) {
         App main = new App();
         try {
-            int resultado = main.calcularNota(63);
+            int resultado = main.calcularNota(nota);
         }
         catch (IllegalArgumentException e) {
             Assertions.assertEquals("Error al calcular la nota", e.getMessage());
